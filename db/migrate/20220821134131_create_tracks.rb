a5856787e56ab3a17725a032ad8a3d675e2bc5c4
João Paulo Dias França <j.pdf00@gmail.com>
2022-08-22 06:56:11 -0300

    Creates schema file

--- db/migrate/20220821134131_create_tracks.rb
+++ db/migrate/20220821134131_create_tracks.rb
@@ -2,7 +2,7 @@ class CreateTracks < ActiveRecord::Migration[6.1]
   def change
     create_table :tracks do |t|
       t.integer :name, index: true
-      t.conference :references
+      t.references :conference
 
       t.timestamps
     end
