94b460c1f77a0f787fca8299b65647408a9822bb
João Paulo Dias França <j.pdf00@gmail.com>
2022-08-22 10:09:00 -0300

    Finishes the file parsing service

--- app/service/file_parsing.rb
+++ app/service/file_parsing.rb
@@ -1,15 +1,19 @@
 class FileParsing
   def initialize(file:)
     @file = file
-    @parsed_info = []
+    @parsed_file = []
   end
 
   def parse
     File.foreach( @file.path ).with_index do |line, index|
-      @parsed_info << line.match(/\d+/).to_s.to_i
+      parsed_line = {}
+      parsed_line[:title] = line.match(/\D+/).to_s.strip
+      parsed_line[:duration] = line.match?('lightning') ? 5 : line.match(/\d+/).to_s.to_i
+
+      @parsed_file << parsed_line
     end
 
-    @parsed_info
+    @parsed_file
   end
 
 end
