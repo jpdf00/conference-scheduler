c071974390d297d95197223a2b3f0ed9d8926555
João Paulo Dias França <j.pdf00@gmail.com>
2022-08-23 12:34:23 -0300

    Refactors associations

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,8 @@
 class Track < ApplicationRecord
+  enum name: ('A'..'Z').to_a
+
   belongs_to :conference
+  has_many :lectures, dependent: :destroy
+
+  accepts_nested_attributes_for :lectures
 end
